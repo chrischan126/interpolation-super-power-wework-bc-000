4ff1075fd3241447eced7a4f4b4b5c169884e9c9
Christopher Chan <chris.chan@wework.com>
2018-02-13 05:27:03 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,10 +2,10 @@
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
+puts 'prints out the colors of the rainbow correctly when passed in in order'
 
 
 def display_rainbow (colors)
-  puts 'prints out the colors of the rainbow correctly when passed in in order'
   
   print "R: #{colors[0]}, "
   print "O: #{colors[1]}, "
